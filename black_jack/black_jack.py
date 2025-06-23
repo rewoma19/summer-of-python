f6d24c46a36f97a6c4751682bac02fd7c6d3eb92
Raphael <rewoma19@gmail.com>
2025-06-22 23:20:40 -0600

    Add code for 4th blackjack task

--- black_jack/black_jack.py
+++ black_jack/black_jack.py
@@ -76,3 +76,27 @@ def value_of_ace(card_one, card_two):
         ace_value = 11
 
     return ace_value
+
+def is_blackjack(card_one, card_two):
+    """
+        Determine if the hand is a 'natural' or 'blackjack'.
+
+        :param card_one, card_two: str - card dealt. See below for values.
+        :return: bool - if the hand is a blackjack (two cards worth 21).
+
+        1. 'J', 'Q', or 'K' (otherwise known as "face cards") = 10
+        2. 'A' (ace card) = 11 (if already in hand)
+        3. '2' - '10' = numerical value.
+    """
+
+    is_blackjack_hand = False
+    player_hand = [card_one, card_two]
+    ten_cards = {'10', 'J', 'K', 'Q'}
+
+    is_ace_in_hand = 'A' in player_hand
+    is_ten_card_in_hand = card_one in ten_cards or card_two in ten_cards
+
+    if is_ace_in_hand and is_ten_card_in_hand:
+        is_blackjack_hand = True
+
+    return is_blackjack_hand
