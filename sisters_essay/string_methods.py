6cdc4ebec85f702a6458584b03416321f86e1c30
Raphael <rewoma19@gmail.com>
2025-06-24 23:42:41 -0600

    Add code for 1st essay task

---
+++ sisters_essay/string_methods.py
@@ -0,0 +1,10 @@
+def capitalize_title(title):
+    """
+        Convert the first letter of each word in the title to uppercase if needed.
+
+        :param title: str - title string that needs title casing.
+        :return: str - title string in title case (first letters capitalized).
+    """
+
+    capitalized_title = title.title()
+    return capitalized_title
