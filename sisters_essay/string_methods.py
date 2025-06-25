d31345a9430ee9f1b2644ccffccb7c9513ec56dd
Raphael <rewoma19@gmail.com>
2025-06-24 23:57:54 -0600

    Add code for final essay task

--- sisters_essay/string_methods.py
+++ sisters_essay/string_methods.py
@@ -29,3 +29,16 @@ def clean_up_spacing(sentence):
 
     cleaned_up_sentence = sentence.strip()
     return cleaned_up_sentence
+
+def replace_word_choice(sentence, old_word, new_word):
+    """
+        Replace a word in the provided sentence with a new one.
+
+        :param sentence: str - a sentence to replace words in.
+        :param old_word: str - word to replace.
+        :param new_word: str - replacement word.
+        :return: str - input sentence with new words in place of old words.
+    """
+
+    updated_sentence = sentence.replace(old_word, new_word)
+    return updated_sentence
