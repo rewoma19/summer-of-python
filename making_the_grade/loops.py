79496812c91b06efb9c63fc5ece049fecca4d72e
Raphael <rewoma19@gmail.com>
2025-06-30 16:22:18 -0600

    Add code for 3rd grade making task

--- making_the_grade/loops.py
+++ making_the_grade/loops.py
@@ -33,3 +33,20 @@ def count_failed_students(student_scores):
 
     return students_with_failed_grade
 
+def above_threshold(student_scores, threshold):
+    """
+        Determine how many of the provided student scors were 'the best' based on the provided threshold.
+
+        :param student_scores: list - of integer scores.
+        :param threshold: int - threshold to cross to be the "best" score.
+        :return: list - of integer scores that are at or above the "best" threshold.
+    """
+
+    best_scores = []
+
+    for score in student_scores:
+        if score >= threshold:
+            best_scores.append(score)
+
+    return best_scores
+
